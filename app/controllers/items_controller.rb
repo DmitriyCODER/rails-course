c2788b19a0fee4a5ef028689558cb8a0567d550f
DmitriyVeshnyakov <dmitriyvs85@mail.ru>
2021-09-29 11:12:04 +0700

    Add all methods items_controller and render and redirect_to

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -4,17 +4,51 @@ class ItemsController < ApplicationController
 
   def index
     @items = Item.all
-    render plain: @items.map { |i| "#{i.name}:#{i.price}" }.join(', ')
   end
 
   def create
     item = Item.create(items_params)
     if item.persisted?
-      render json: item.name, status: :created
+      redirect_to items_path
     else
       render json: item.errors, status: :unprocessable_entity
     end
 
+  end
+
+  def new
+
+  end
+
+  def show
+    unless @item = Item.where(id: params[:id]).first
+      render body: 'Page not found', status: 404
+    end
+  end
+
+  def edit
+    unless @item = Item.where(id: params[:id]).first
+      render body: 'Page not found', status: 404
+    end
+  end
+
+  def update
+    item = Item.where(id: params[:id]).first
+    if item.update(items_params)
+      redirect_to item_path
+    else
+      render json: item.errors, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    item = Item.where(id: params[:id]).first.destroy
+
+    if item.destroyed?
+      redirect_to items_path
+    else
+      render json: item.errors, status: :unprocessable_entity
+    end
   end
 
   private
